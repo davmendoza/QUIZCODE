91ab96b033e7d32618f4f1ec5ab0b0fb87425ec9
david mendoza <davidsmacbook@davids-mbp.attlocal.net>
2020-11-17 17:35:59 -0800

    edited the questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,14 +1,14 @@
 
 var questions = [
   {
-    title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
+    title: "Which of the following tags is used to insert a blank line?",
+    choices: ["<br>", "<h1>", "<hr>", "<p>"],
+    answer: "<br>"
   },
   {
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
+    title: "Which HTML tag is used to define an internal style sheet?",
+    choices: ["<css>", "<style>", "<iss>", "<script>"],
+    answer: "<style>"
   },
   {
     title: "Arrays in JavaScript can be used to store ____.",
